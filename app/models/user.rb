a9cd0a1e7615fda9c2da8ad2d8839870ed5f70ab
rsundar <rohan.s.1987@gmail.com>
2020-05-06 22:17:55 +0530

    Finished user model and added links to sign-in, sign-up & log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   before_validation :set_default_password
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   private
 
   def set_default_password
