3ae8d6aebbc14a3056c50914b6228c9efd8d4c46
Mario Barrios <mariobarriossalazar@outlook.com>
2020-05-06 12:42:25 -0600

    Added helper method for events and added routes for attend_event and cancel_attendance actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/event', to: 'events#attend_event'
+  delete '/event', to: 'events#cancel_attendance'
   devise_for :users
   resources :users, only: [:show]
   resources :events
