1e6dfe58e0ae94a14e6be347bddeb3f15f557c11
Mario Barrios <mariobarriossalazar@outlook.com>
2020-05-06 12:10:36 -0600

    Added associations between Event model and EventAttendance

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+  has_many :attendees, through: :event_attendances, source: :attendee
+  has_many :event_attendances, foreign_key: :attended_event_id
 end
