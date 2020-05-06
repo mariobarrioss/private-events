c613fd2d2316c0e90cbcfe56ad28383fbd60650f
Mario Barrios <mariobarriossalazar@outlook.com>
2020-05-06 12:27:17 -0600

    Added _event partial, and modified user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @events_created_by_user = @user.events.order(created_at: :desc)
+
+    attending_events = Event.joins(:event_attendances).where("event_attendances.attendee_id = #{@user.id}")
+    @past_events = attending_events.past
+    @upcoming_events = attending_events.upcoming
   end
 end
